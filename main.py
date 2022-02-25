9b39ed179c16340e515cfe7437acadbc95fce945
kiahdd <kianahaddadi@gmail.com>
2022-02-25 10:21:18 -0500

    tweet scraper is done! move to sql database

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#%%
 import key as c
 import tweepy
 import sqlalchemy 
@@ -6,15 +5,14 @@ from pprint import pprint
 from textblob import TextBlob
 from nltk.corpus import stopwords
 import re
-import sys
 
 # constants
 consumer_key            = c.API_KEY
 consumer_secret         = c.API_KEy_SECRET
 access_token            = c.Access_token
 access_token_secret     = c.Access_token_secret
-tweetsPerQry            = 1
-maxTweets               = 100000
+tweetsPerQry            = 100
+maxTweets               = 100
 hashtag                 = "#InternetShutDown"
 
 # Data processing: Cleaning Tweet texts
@@ -31,7 +29,13 @@ def clean_text(new_tweet):
 
 
 def sentiment_score(new_tweet):
-    pass
+    analysis = TextBlob(new_tweet)
+    if analysis.sentiment.polarity > 0:
+        return 1 #positive
+    elif analysis.sentiment.polarity == 0: #neutrial
+        return 0
+    else:
+        return -1  #negative
 
 
 def main():
@@ -43,37 +47,66 @@ def main():
     api  = tweepy.API(auth)
     api = tweepy.API(auth, wait_on_rate_limit=True)
     # Get the User object for twitter...
-    maxId = -1
+    maxId = -1  # initialization
     tweetCount = 0
     meta_list = []
+    
+    while tweetCount< maxTweets:
+        newTweets = []
 
-    if(maxId <= 0):
-        newtweets = api.search_tweets(q=hashtag,count=tweetsPerQry, result_type="recent",tweet_mode="extended")
-    else: 
-        newTweets = api.search(q=hashtag, count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent", tweet_mode="extended")
+        if(maxId <= 0): # reteriving the most recent tweets
+            print("Retrieving  the most recent tweets ...")
+            newTweets = api.search_tweets(q=hashtag,count=tweetsPerQry, result_type="recent",tweet_mode="extended")
+        else:  
+            print("Retrieving older tweets ...")
+            # Returns only statuses with an ID less than (that is, older than) or equal to the specified ID
+            newTweets = api.search_tweets(q=hashtag, count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent", tweet_mode="extended")
 
-    if not newTweets:
-        print("Done")
-        sys.exit(0)
-    for tweet in newtweets:
-        username = tweet.user.name
-        created_at = str(tweet.created_at)
+        if not newTweets:
+            print("Done!")
+            break
 
-        try:
-            tweet_text = tweet.retweeted_status.full_text
-        except AttributeError:  # Not a Retweet
-            tweet_text = tweet.full_text
-        tweet_text = tweet.retweeted_status.full_text
-        tweet_text_sent = tweet_text
-        retweet_count = tweet.retweet_count
-        fav_count = tweet.favorite_count
-        media_source = tweet.source
-        tweet_text_sent = clean_text(tweet_text_sent)
-        print(tweet_text)
-        pprint(tweet_text_sent)
-        print(retweet_count)
-        print(media_source)
-# main()
+        for tweet in newTweets:
+            username = tweet.user.name  #username
+            created_at = str(tweet.created_at)  # when it was posted?
+
+            #obtain full tex for retweets and tweets
+            try:
+                tweet_text = tweet.retweeted_status.full_text   # full text of retweet
+                continue   # if it was a retweet skip
+
+            except AttributeError:  # Not a Retweet
+                tweet_text = tweet.full_text # full text of tweet
+
+            tweet_text_sent = tweet_text    
+            retweet_count = tweet.retweet_count  
+            fav_count = tweet.favorite_count
+            media_source = tweet.source
+
+            # cleaning tweet text to remove hashtages
+            tweet_text_sent = clean_text(tweet_text_sent)
+
+            # sentiment score analysis
+            result_score = sentiment_score(tweet_text_sent)
+
+            #convert to dic
+            data_dict = {
+            "username":username,
+            "created_at":created_at,
+            "tweet_text":tweet_text,
+            "retweet_count":retweet_count,
+            "fav_count":fav_count,
+            "media_source":media_source,
+            "sentiment_score":result_score
+            }
+            print(data_dict["sentiment_score"])
+            meta_list.append(data_dict)
+        # end for
+        tweetCount += len(newTweets)
+        maxId = newTweets[-1].id
+    
+
+main()
 
 
 
